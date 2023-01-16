cea98576a470c1fd860d1d4cf83d4e59beab2923
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2023-01-16 10:57:15 -0500

    lesson 1

---
+++ code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Marshall Demars and Cameron Diedrich
+# Created on: October 2022
+# This program is the "David Dash" program on the PyBadge
+
+
+import ugame
+import stage
+
+
+def game_scene():
+   # this function is the main game game_scene
+
+   print("\n\n\n") # 3 blank lines
+   print("Hello, World!")
+
+   # repeat forever, game loop
+   while True:
+       pass # just a placeholder for now
+
+
+if __name__ == "__main__":
+   game_scene()
