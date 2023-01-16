209cc4416c9eaedf24a4278f4c0c572171d2340b
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2023-01-16 11:14:55 -0500

    changes

---
+++ lesson 2/code.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Created by: Marshall Demars and Cameron Diedrich
+# Created on: Jan 2023
+# This program is the "David Dash" program on the PyBadge
+
+
+import stage
+import ugame
+
+
+def game_scene():
+   # this function is the main game game_scene
+
+   # image banks for CircuitPython
+   image_bank_background = stage.Bank.from_bmp16("water,jpg")
+
+   # set the background to image 0 in the image bank
+   #    and the size (10x8 tiles of size 16x16)
+   background = stage.Grid(image_bank_background, 10, 8)
+
+   # create a stage for the background to show up on
+   #    and set the frame rate to 60fps
+   game = stage.Stage(ugame.display, 60)
+   # set the layers of all sprites, items show up in order
+   game.layers = [background]
+   # render all sprites
+   #    most likely you will only render the background once per game scene
+   game.render_block()
+
+   # repeat forever, game loop
+   while True:
+       pass  # just a placeholder for now
+
+
+if __name__ == "__main__":
+   game_scene()
