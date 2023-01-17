a90a8f5247abfff8f2aac1af149a18fef31d5b68
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2023-01-17 10:26:05 -0500

    fix

--- lesson 2/code.py
+++ lesson 2/code.py
@@ -13,7 +13,11 @@ def game_scene():
    # this function is the main game game_scene
 
    # image banks for CircuitPython
-   image_bank_background = ("water,jpg")
+   image_bank_background = stage.Bank.from_bmp16("water.bmp")
+
+   # set the background to image 0 in the image bank
+   #    and the size (10x8 tiles of size 16x16)
+   background = stage.Grid(image_bank_background, 10, 8)
 
    # create a stage for the background to show up on
    #    and set the frame rate to 60fps
