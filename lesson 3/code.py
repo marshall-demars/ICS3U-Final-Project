310ac8b33e45c850eeb64c4954def5508e7be4cc
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2023-01-18 11:14:23 -0500

    fix

--- lesson 3/code.py
+++ lesson 3/code.py
@@ -2,7 +2,7 @@
 
 # Created by: Marshall Demars and Cameron Diedrich
 # Created on: Jan 2023
-# This program is the "Ball Game" program on the PyBadge
+# This program is the "David Dash" program on the PyBadge
 
 
 import stage
@@ -14,23 +14,35 @@ def game_scene():
 
    # image banks for CircuitPython
    image_bank_background = stage.Bank.from_bmp16("ball.bmp")
+   image_bank_sprites = stage.Bank.from_bmp16("ball.bmp")
 
    # set the background to image 0 in the image bank
    #    and the size (10x8 tiles of size 16x16)
    background = stage.Grid(image_bank_background, 10, 8)
 
+   # a sprite that will be updated every frame
+   ball = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
    # create a stage for the background to show up on
    #    and set the frame rate to 60fps
    game = stage.Stage(ugame.display, 60)
+
    # set the layers of all sprites, items show up in order
-   game.layers = [background]
+   game.layers = [ball] + [background]
+
    # render all sprites
    #    most likely you will only render the background once per game scene
    game.render_block()
 
    # repeat forever, game loop
    while True:
-       pass  # just a placeholder for now
+       # get user input
+
+       # update game logic
+
+       # redraw Sprite
+       game.render_sprites([ball])
+       game.tick()  # wait until refresh rate finishes
 
 
 if __name__ == "__main__":
