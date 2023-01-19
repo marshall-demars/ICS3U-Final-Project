c3df97c080ee07b7eb7512bd0ea29cd5c54e1a05
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2023-01-19 10:25:18 -0500

    fix

---
+++ lesson 4/code.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+# Created by: Marshall Demars and Cameron Diedrich
+# Created on: Jan 2023
+# This program is the "David Dash" program on the PyBadge
+
+
+import stage
+import ugame
+
+
+def game_scene():
+    # this function is the main game game_scene
+
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("ball.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("ball.bmp")
+
+    # set the background to image 0 in the image bank
+    #    and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    # a sprite that will be updated every frame
+    ball = stage.Sprite(image_bank_sprites, 2, 75, 66)
+
+    # create a stage for the background to show up on
+    #    and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+
+    # set the layers of all sprites, items show up in order
+    game.layers = [ball] + [background]
+
+    # render all sprites
+    #    most likely you will only render the background once per game scene
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+
+        # update game logic
+
+        # redraw Sprite
+        game.render_sprites([ship])
+        game.tick()  # wait until refresh rate finishes
+
+
+if __name__ == "__main__":
+    game_scene()
