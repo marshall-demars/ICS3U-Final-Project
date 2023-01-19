fb1db9cf355dabdf5f1d544a14f11768dc7ccc71
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2023-01-19 11:18:57 -0500

    08

--- lesson 8/code.py
+++ lesson 8/code.py
@@ -133,11 +133,6 @@ def game_scene():
     # set the background to image 0 in the image bank
     background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X,
         constants.SCREEN_GRID_Y)
-    for x_location in range(constants.SCREEN_GRID_X):
-        for y_location in range(constants.SCREEN_GRID_Y):
-            tile_picked = random.randint(1, 3)
-            background.tile(x_location, y_location, tile_picked)
-
 
     # a sprite that will be updated every frame
     ball = stage.Sprite(
